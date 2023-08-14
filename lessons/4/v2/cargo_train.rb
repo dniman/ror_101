bf80eba1b37d17534f5ae890a76fa1f45870876f
Dade Niman <dade.niman@gmail.com>
2023-08-14 12:27:37 +0300

    correct

--- lessons/4/v2/cargo_train.rb
+++ lessons/4/v2/cargo_train.rb
@@ -6,7 +6,7 @@ require_relative 'cargo_carriage'
 class CargoTrain < Train
   attr_reader :type
 
-  def initialize(number, _type)
+  def initialize(number)
     super(number)
     @type = :cargo
   end
